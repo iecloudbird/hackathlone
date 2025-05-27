5342f6d3773f23ee7c22340f5ee2be3138f98f3c
iecloudbird <kuroxxxshiro9@gmail.com>
2025-05-28 00:10:45 +0100

    done

--- apps/current/prettier.config.mjs
+++ apps/current/prettier.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
-    plugins: ["prettier-plugin-tailwindcss"],
-    tailwindConfig: "./tailwind.config.ts",
-    bracketSpacing: true,
-    printWidth: 80,
-    semi: true,
-    singleQuote: false,
-    tabWidth: 2,
-    trailingComma: "es5",
-    useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
 };
 
 export default config;
