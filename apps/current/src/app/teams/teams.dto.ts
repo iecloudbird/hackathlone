3ac3a2075eb34eab8c8d68a7d9c4df26412e8162
Shantanu <shantanukhoslas@gmail.com>
2025-10-04 13:00:08 +0530

    Updated Volunteers

--- apps/current/src/app/teams/teams.dto.ts
+++ apps/current/src/app/teams/teams.dto.ts
@@ -2,21 +2,29 @@ import Aaron from "@/assets/images/general/landing-page/teams/Aaron.jpeg";
 import Albert from "@/assets/images/general/landing-page/teams/Albert.jpeg";
 import Babar from "@/assets/images/general/landing-page/teams/Babar.png";
 import Bhagyabati from "@/assets/images/general/landing-page/teams/Bhagyabati.jpg";
+import Danny from "@/assets/images/general/landing-page/teams/Danny.png";
 import Darshana from "@/assets/images/general/landing-page/teams/darshana.png";
 import Dylan from "@/assets/images/general/landing-page/teams/Dylan.png";
 import Fabrizio from "@/assets/images/general/landing-page/teams/fabrizio.png";
 import Harsh from "@/assets/images/general/landing-page/teams/Harsh.jpeg";
 import James from "@/assets/images/general/landing-page/teams/james.png";
+import Joey from "@/assets/images/general/landing-page/teams/Joey.jpeg";
+import Katherine from "@/assets/images/general/landing-page/teams/Katherine_Pang.jpg";
 import Ken from "@/assets/images/general/landing-page/teams/ken.jpg";
 import Manav from "@/assets/images/general/landing-page/teams/Manav_Shah.jpg";
 import Mehwish from "@/assets/images/general/landing-page/teams/Mehwish.jpg";
 // import Michael from "@/assets/images/general/landing-page/teams/Michael.jpg";
+import Micheal_Mahony from "@/assets/images/general/landing-page/teams/Michael_Mahony.jpg";
+import Micheal_Gill from "@/assets/images/general/landing-page/teams/Micheál_Gill.jpg";
+import Navoda from "@/assets/images/general/landing-page/teams/Navoda.jpg";
 import Nitish from "@/assets/images/general/landing-page/teams/Nitish.jpg";
 import Pitor from "@/assets/images/general/landing-page/teams/pitor.png";
 import Pratik from "@/assets/images/general/landing-page/teams/Pratik.jpeg";
+import Patrik from "@/assets/images/general/landing-page/teams/Pratik_Mote.jpg";
 import Sahir from "@/assets/images/general/landing-page/teams/Sahir Sharma.jpg";
 import Sanjana from "@/assets/images/general/landing-page/teams/Sanjana.jpeg";
 import Shean from "@/assets/images/general/landing-page/teams/Shean Hans Teoh.jpg";
+import Shu from "@/assets/images/general/landing-page/teams/Shu.jpg";
 import Shantanu from "@/assets/images/general/landing-page/teams/sk.jpg";
 import Yujing from "@/assets/images/general/landing-page/teams/Yujing.png";
 
@@ -103,17 +111,6 @@ export const teamMembers = [
       twitter: null,
     },
   },
-  // {
-  //   bio: null,
-  //   image: Kkwaish,
-  //   name: "Kkhawaish Gulati",
-  //   role: "Event Anchor",
-  //   socials: {
-  //     github: null,
-  //     linkedin: "https://www.linkedin.com/in/kkhawaish-gulati-050b511ab/",
-  //     twitter: null,
-  //   },
-  // },
   {
     bio: null,
     image: Ken,
@@ -131,92 +128,218 @@ export const teamMembers = [
   {
     bio: null,
     image: Aaron,
-    name: "Aaron",
+    name: "Aaron McEvoy",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/aaron-mcevoy-238658225/",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Albert,
-    name: "Albert",
+    name: "Albert Luganga",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/albertluganga/",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Babar,
-    name: "Babar",
+    name: "Muhammad Babar Imtiaz",
     role: "On-Ground Support",
     socials: { github: null, linkedin: null, twitter: null },
   },
   {
     bio: null,
     image: Bhagyabati,
-    name: "Bhagyabati",
+    name: "Bhagyabati Moharana",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/bhagyabati-m-a07619172/",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Darshana,
-    name: "Darshana",
+    name: "Darshana Devda",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/darshana-devda",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Dylan,
-    name: "Dylan",
+    name: "Dylan Lowe",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/dylan-l-3974b5225",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Fabrizio,
-    name: "Fabrizio",
+    name: "Fabrizio De Fiore",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/fabrizio-de-fiore/",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Harsh,
-    name: "Harsh",
+    name: "Harsh Lotiya",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/harsh-lotiya",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Mehwish,
-    name: "Mehwish",
+    name: "Mehwish Tahir",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/mehwish-tahir-b7980417",
+      twitter: null,
+    },
   },
-  // {
-  //   bio: null,
-  //   image: Michael,
-  //   name: "Michael",
-  //   role: "On-Ground Support",
-  //   socials: { github: null, linkedin: null, twitter: null },
-  // },
   {
     bio: null,
     image: Pratik,
-    name: "Pratik",
+    name: "Pratik Mote",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/pratik-mote",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Sanjana,
-    name: "Sanjana",
+    name: "Sanjana Shashi",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/sanjanashashi",
+      twitter: null,
+    },
   },
   {
     bio: null,
     image: Yujing,
-    name: "Yujing",
+    name: "Yujing Zhang",
     role: "On-Ground Support",
-    socials: { github: null, linkedin: null, twitter: null },
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/yujingzhang/",
+      twitter: null,
+    },
+  },
+  /* New mentors to be added */
+  {
+    bio: null,
+    image: Danny,
+    name: "Danny Hewitt",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/danny-hewitt-tusathlone/",
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Patrik, // Need Saumya image
+    name: "Saumya Shashi",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/saumyashashi/",
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Micheal_Mahony,
+    name: "Michael Mahony",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/michael-mahony-04407b295/",
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Navoda,
+    name: "Jayasekara Kapukotuwa",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/kapukotuwa/",
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Joey, // Need Zheng Yi Lee image
+    name: "Zheng Yi Lee",
+    role: "Event Photographer",
+    socials: {
+      github: null,
+      linkedin: null,
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Shu,
+    name: "Shu Wen Yap",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: null,
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Katherine,
+    name: "Katherine Yuan Yi Pang",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: "https://www.linkedin.com/in/katherine-pang-8b42892b2/",
+      twitter: null,
+    },
+  },
+  {
+    bio: null,
+    image: Micheal_Gill,
+    name: "Micheál Gill",
+    role: "On-Ground Support",
+    socials: {
+      github: null,
+      linkedin: null,
+      twitter: null,
+    },
   },
 ];
